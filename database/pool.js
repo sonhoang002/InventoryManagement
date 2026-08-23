fc4caef6374aec2fd26874f18d269a2095158839
SonHoang <120953254+sonhoang002@users.noreply.github.com>
2026-08-22 22:12:17 -0400

    Updating pool

--- database/pool.js
+++ database/pool.js
@@ -1,9 +1,13 @@
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: "localhost",
-  user: "sh0101",
-  password: process.env.LOCAL_DB_PASSWORD,
-  database: "maple",
-  port: 5432,
-});
+module.exports = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+    })
+  : new Pool({
+      host: process.env.LOCAL_DB_HOST || "localhost",
+      port: Number(process.env.LOCAL_DB_PORT) || 5432,
+      user: process.env.LOCAL_DB_USER,
+      password: process.env.LOCAL_DB_PASSWORD,
+      database: process.env.LOCAL_DB_NAME || "maple",
+    });
